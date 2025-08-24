798dd137be627d0c14f2a061e86cb4853c25ef4f
ntdyx <nataliadiaspp@gmail.com>
2025-08-24 13:42:41 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// =======================================================
+// ARQUIVO DE CONFIGURAÇÃO PRINCIPAL
+// =======================================================
+const CONFIG = {
+    GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycby5ctQdcvZaronRx0nSmVS-4PwXf0acZgoLhWZeZXYnn7moVDvlyPiXQ9T_qzpCVpBVTw/exec',
+    
+    APP_NAME: 'Bet Baby',
+    VERSION: '1.0.0'
+};
